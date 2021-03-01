5c39f0f867e956780a4c93e505d82f1a048e572b
nomaksim <nmaximd@gmail.com>
2021-03-01 11:01:14 +0600

    Basic auth, UserSeeder

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return app()->version();
-});
+Route::get('/', 'HomeController@index')->name('home');
+
+Auth::routes();
